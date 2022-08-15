839a78aebea2e0e338717676c37b93e17ab0cab9
Matthew Belcher <matthewjbelcher7@live.com>
2022-08-15 15:23:57 -0600

    WIP
    Co-authored-by: Athea DeLing <almdeling@gmail.com>
    Co-authored-by: Ted Gold <ted.jonathan.gold@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,12 @@
-form.onsubmit = function
-const favoriteThings = ["favoriteColor", "favoriteSeason", "favoriteNinjaTurtle", "favoriteIceCream", "favoriteAnimal"]
+window.onload = function() {
+  const form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    event.preventDefault();
+   // const favoriteThings = ["favoriteColor", "favoriteSeason", "favoriteNinjaTurtle", "favoriteIceCream", "favoriteAnimal"]
+  
+    //> document.getElementsByName("favoriteColor").innerText = favoriteColor;
+    <input id="person1Input" type="text" name="person1Input">
+  
+  
+  };
+}
